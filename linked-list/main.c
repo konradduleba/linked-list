5048086d510b1024f7c64be333ee4ce043e2a85d
Konrad Dulęba <konrad.duleba@miquido.com>
2023-01-09 21:31:02 +0100

    Initial Commit

---
+++ linked-list/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  linked-list
+//
+//  Created by Konrad on 09/01/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
