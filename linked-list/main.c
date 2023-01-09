8513d3745feba8b39d95214c53ca06dcb8f5bb26
Konrad Dulęba <konrad.duleba@>
2023-01-09 21:34:13 +0100

    Removed unneeded comments

--- linked-list/main.c
+++ linked-list/main.c
@@ -1,14 +1,6 @@
-//
-//  main.c
-//  linked-list
-//
-//  Created by Konrad on 09/01/2023.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(void) {
     printf("Hello, World!\n");
     return 0;
 }
